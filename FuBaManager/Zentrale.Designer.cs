af5c1f366f20eb380bb89589dd59d3a9c61138fb
BuildTools <unconfigured@null.spigotmc.org>
2017-08-17 00:24:49 +0200

    v preAlph 2 -Grundlegendes Zeitsystem-

--- FuBaManager/Zentrale.Designer.cs
+++ FuBaManager/Zentrale.Designer.cs
@@ -41,6 +41,14 @@
             this.toolStripTextBox6 = new System.Windows.Forms.ToolStripTextBox();
             this.ContentPanel = new System.Windows.Forms.ToolStripContentPanel();
             this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.Weiter = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.toolStripContainer1.LeftToolStripPanel.SuspendLayout();
             this.toolStripContainer1.SuspendLayout();
@@ -91,21 +99,21 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.menuStrip1.Size = new System.Drawing.Size(100, 142);
+            this.menuStrip1.Size = new System.Drawing.Size(98, 144);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // toolStripTextBox1
             // 
             this.toolStripTextBox1.Name = "toolStripTextBox1";
-            this.toolStripTextBox1.Size = new System.Drawing.Size(91, 23);
+            this.toolStripTextBox1.Size = new System.Drawing.Size(89, 23);
             this.toolStripTextBox1.Text = "Transfermarkt";
             this.toolStripTextBox1.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // toolStripTextBox2
             // 
             this.toolStripTextBox2.Name = "toolStripTextBox2";
-            this.toolStripTextBox2.Size = new System.Drawing.Size(91, 23);
+            this.toolStripTextBox2.Size = new System.Drawing.Size(89, 23);
             this.toolStripTextBox2.Text = "Mannschaften";
             this.toolStripTextBox2.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.toolStripTextBox2.Click += new System.EventHandler(this.toolStripTextBox2_Click);
@@ -113,21 +121,21 @@
             // toolStripTextBox3
             // 
             this.toolStripTextBox3.Name = "toolStripTextBox3";
-            this.toolStripTextBox3.Size = new System.Drawing.Size(91, 23);
+            this.toolStripTextBox3.Size = new System.Drawing.Size(89, 23);
             this.toolStripTextBox3.Text = "Finanzen";
             this.toolStripTextBox3.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // toolStripTextBox4
             // 
             this.toolStripTextBox4.Name = "toolStripTextBox4";
-            this.toolStripTextBox4.Size = new System.Drawing.Size(91, 23);
+            this.toolStripTextBox4.Size = new System.Drawing.Size(89, 23);
             this.toolStripTextBox4.Text = "Vereinsgelände";
             this.toolStripTextBox4.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // toolStripTextBox5
             // 
             this.toolStripTextBox5.Name = "toolStripTextBox5";
-            this.toolStripTextBox5.Size = new System.Drawing.Size(91, 23);
+            this.toolStripTextBox5.Size = new System.Drawing.Size(89, 23);
             this.toolStripTextBox5.Text = "Verein";
             this.toolStripTextBox5.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.toolStripTextBox5.Click += new System.EventHandler(this.toolStripTextBox5_Click);
@@ -135,7 +143,7 @@
             // toolStripTextBox6
             // 
             this.toolStripTextBox6.Name = "toolStripTextBox6";
-            this.toolStripTextBox6.Size = new System.Drawing.Size(91, 23);
+            this.toolStripTextBox6.Size = new System.Drawing.Size(89, 23);
             this.toolStripTextBox6.Text = "Statistik";
             this.toolStripTextBox6.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -149,23 +157,104 @@
             // 
             // toolStripContainer1.ContentPanel
             // 
-            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(6, 142);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(1, 144);
             // 
             // toolStripContainer1.LeftToolStripPanel
             // 
             this.toolStripContainer1.LeftToolStripPanel.Controls.Add(this.menuStrip1);
-            this.toolStripContainer1.Location = new System.Drawing.Point(3, 3);
+            this.toolStripContainer1.Location = new System.Drawing.Point(3, 152);
             this.toolStripContainer1.Name = "toolStripContainer1";
             this.toolStripContainer1.RightToolStripPanelVisible = false;
-            this.toolStripContainer1.Size = new System.Drawing.Size(106, 142);
+            this.toolStripContainer1.Size = new System.Drawing.Size(99, 144);
             this.toolStripContainer1.TabIndex = 1;
             this.toolStripContainer1.Text = "toolStripContainer1";
             this.toolStripContainer1.TopToolStripPanelVisible = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(910, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(90, 80);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(910, 86);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(90, 80);
+            this.richTextBox2.TabIndex = 8;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox3.Location = new System.Drawing.Point(910, 172);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(90, 80);
+            this.richTextBox3.TabIndex = 9;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox7.Location = new System.Drawing.Point(910, 516);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(90, 80);
+            this.richTextBox7.TabIndex = 10;
+            this.richTextBox7.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox6.Location = new System.Drawing.Point(910, 430);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(90, 80);
+            this.richTextBox6.TabIndex = 11;
+            this.richTextBox6.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox5.Location = new System.Drawing.Point(910, 344);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(90, 80);
+            this.richTextBox5.TabIndex = 12;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox4.Location = new System.Drawing.Point(910, 258);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(90, 80);
+            this.richTextBox4.TabIndex = 13;
+            this.richTextBox4.Text = "";
+            // 
+            // Weiter
+            // 
+            this.Weiter.Location = new System.Drawing.Point(0, 0);
+            this.Weiter.Name = "Weiter";
+            this.Weiter.Size = new System.Drawing.Size(95, 51);
+            this.Weiter.TabIndex = 14;
+            this.Weiter.Text = "Weiter";
+            this.Weiter.UseVisualStyleBackColor = true;
+            this.Weiter.Click += new System.EventHandler(this.Weiter_Click);
             // 
             // Zentrale
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Weiter);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.toolStripContainer1);
             this.Name = "Zentrale";
             this.Size = new System.Drawing.Size(1000, 600);
@@ -195,5 +284,13 @@
         private System.Windows.Forms.ToolStripTextBox toolStripTextBox6;
         private System.Windows.Forms.ToolStripContentPanel ContentPanel;
         private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Button Weiter;
     }
 }
--- FuBaManager/Zentrale.cs
+++ FuBaManager/Zentrale.cs
@@ -16,17 +16,19 @@ namespace FuBaManager
     {
 
         public Mannschaft1 mannschaft1 { get; internal set; }
-
+        // Zeit Verlegen -- Database?? --
+        public DateTime Zeit = new DateTime(2016, 8, 1);
+        DateTime Zeit_2 = new DateTime();
+        DateTime Zeit_3 = new DateTime();
+        DateTime Zeit_4 = new DateTime();
+        DateTime Zeit_5 = new DateTime();
+        DateTime Zeit_6 = new DateTime();
+        DateTime Zeit_7 = new DateTime();
         public Zentrale()
         {
             InitializeComponent();
             
-            //this.dataGridView1.AutoGenerateColumns = true;
-
-            //_teamsModel = new TeamsModel();
-            //_teamsModel.Teams.Load();
-
-            //this.bindingSource1.DataSource = _teamsModel.Teams.Local.ToBindingList();
+            
         }
 
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
@@ -58,7 +60,35 @@ namespace FuBaManager
 
         private void Zentrale_Load(object sender, EventArgs e)
         {
+            Zeit_tick();
+        }
 
+        private void Weiter_Click(object sender, EventArgs e)
+        {
+            Zeit = Zeit.AddDays(1);
+            Zeit_tick();
+        }
+        private void Zeit_tick()
+        {
+            richTextBox1.Text = Zeit.ToShortDateString();
+
+            Zeit_2 = Zeit.AddDays(1);
+            richTextBox2.Text = Zeit_2.ToShortDateString();
+
+            Zeit_3 = Zeit.AddDays(2);
+            richTextBox3.Text = Zeit_3.ToShortDateString();
+
+            Zeit_4 = Zeit.AddDays(3);
+            richTextBox4.Text = Zeit_4.ToShortDateString();
+
+            Zeit_5 = Zeit.AddDays(4);
+            richTextBox5.Text = Zeit_5.ToShortDateString();
+
+            Zeit_6 = Zeit.AddDays(5);
+            richTextBox6.Text = Zeit_6.ToShortDateString();
+
+            Zeit_7 = Zeit.AddDays(6);
+            richTextBox7.Text = Zeit_7.ToShortDateString();
         }
     }
 }
